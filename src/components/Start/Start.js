8c781d9661403e0fc0724a2cda2b6db54aa5fbff
MartaSolo <marciamarcia@o2.pl>
2022-04-28 19:44:16 +0200

    Added styling for Start for desktop

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -6,26 +6,27 @@ export const Start = () => {
   return (
     <section id="start" className="start">
       <div className="start__content">
-        <h1 className="start__content-title">Zacznij pomagać!</h1>
-        <h2 className="start__content-subtitle">
-          Oddaj niechciane rzeczy w zaufane ręce
-        </h2>
-        <div className="start__content-decoration"></div>
-      </div>
+        <div className="start__text">
+          <h1 className="start__text-title">Zacznij pomagać!</h1>
+          <h2 className="start__text-subtitle">
+            Oddaj niechciane rzeczy w zaufane ręce
+          </h2>
+          <div className="start__text-decoration"></div>
+        </div>
 
-      <div className="start__links">
-        <NavLink to="/login" className="start__links-login">
-          Oddaj
-          <br />
-          rzeczy
-        </NavLink>
-        <NavLink to="/login" className="start__links-login">
-          Zorganizuj
-          <br />
-          zbiórkę
-        </NavLink>
+        <div className="start__links">
+          <NavLink to="/login" className="start__links-login">
+            Oddaj
+            <br />
+            rzeczy
+          </NavLink>
+          <NavLink to="/login" className="start__links-login">
+            Zorganizuj
+            <br />
+            zbiórkę
+          </NavLink>
+        </div>
       </div>
-
       <div className="start__image" alt="box with used stuff"></div>
     </section>
   );
