a9617cec8f6d6111b77e8a6f09b5927d4a381c3a
MartaSolo <marciamarcia@o2.pl>
2022-04-28 15:06:34 +0200

    editing NavLinks component - onClick={closeHandler} deleted from li element , added to Link from react-scroll

--- src/components/NavBar/NavLinks/NavLinks.js
+++ src/components/NavBar/NavLinks/NavLinks.js
@@ -5,62 +5,68 @@ import { Link } from "react-scroll";
 export const NavLinks = ({ isMobile, closeMobileMenu }) => {
   const clickHandler = () => {
     if (typeof closeMobileMenu === "function" && isMobile === true) {
+      console.log("clickhandler");
       closeMobileMenu();
     }
   };
   return (
     <ul className="nav__list">
-      <li className="nav__list-item" onClick={clickHandler}>
+      <li className="nav__list-item">
         <Link
           to="start"
           activeClass="active"
           spy={true}
           smooth={true}
           className="nav__list-link"
+          onClick={clickHandler}
         >
           Start
         </Link>
       </li>
-      <li className="nav__list-item" onClick={clickHandler}>
+      <li className="nav__list-item">
         <Link
           to="steps"
           activeClass="active"
           spy={true}
           smooth={true}
           className="nav__list-link"
+          onClick={clickHandler}
         >
           O co chodzi?
         </Link>
       </li>
-      <li className="nav__list-item" onClick={clickHandler}>
+      <li className="nav__list-item">
         <Link
           to="about"
           activeClass="active"
           spy={true}
           smooth={true}
           className="nav__list-link"
+          onClick={clickHandler}
         >
           O nas
         </Link>
       </li>
-      <li className="nav__list-item" onClick={clickHandler}>
+      <li className="nav__list-item">
         <Link
           to="organizations"
           activeClass="active"
           spy={true}
           smooth={true}
           className="nav__list-link"
+          onClick={clickHandler}
         >
           Fundacja i organizacje
         </Link>
       </li>
-      <li className="nav__list-item" onClick={clickHandler}>
+      <li className="nav__list-item">
         <Link
           to="contact"
           activeClass="active"
           spy={true}
           smooth={true}
           className="nav__list-link"
+          onClick={clickHandler}
         >
           Kontakt
         </Link>
