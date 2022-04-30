a10afa27a6a5ed92684966c95614abb172296962
MartaSolo <marciamarcia@o2.pl>
2022-04-30 13:22:50 +0200

    Added react-router-hash-link, changed the NavLinks component by implementing HashLink drom react-router-hach-link instead of Link from react-scroll, removed react-scroll from dependencies

--- src/components/molecules/NavLinks/NavLinks.js
+++ src/components/molecules/NavLinks/NavLinks.js
@@ -1,4 +1,4 @@
-import { Link } from "react-scroll";
+import { HashLink } from "react-router-hash-link";
 import "./NavLinks.scss";
 
 const NavLinks = ({ isMobile, closeMobileMenu }) => {
@@ -10,67 +10,138 @@ const NavLinks = ({ isMobile, closeMobileMenu }) => {
   return (
     <ul className="nav__list">
       <li className="nav__list-item">
-        <Link
-          to="start"
-          activeClass="active"
-          spy={true}
-          smooth={true}
+        <HashLink
+          smooth
+          to="/#start"
+          activeClassName="active"
           className="nav__list-link"
           onClick={closeMenuHandler}
         >
           Start
-        </Link>
+        </HashLink>
       </li>
       <li className="nav__list-item">
-        <Link
-          to="steps"
-          activeClass="active"
-          spy={true}
-          smooth={true}
+        <HashLink
+          smooth
+          to="/#steps"
+          activeClassName="active"
           className="nav__list-link"
           onClick={closeMenuHandler}
         >
           O co chodzi?
-        </Link>
+        </HashLink>
       </li>
       <li className="nav__list-item">
-        <Link
-          to="about"
-          activeClass="active"
-          spy={true}
-          smooth={true}
+        <HashLink
+          smooth
+          to="/#about"
+          activeClassName="active"
           className="nav__list-link"
           onClick={closeMenuHandler}
         >
           O nas
-        </Link>
+        </HashLink>
       </li>
       <li className="nav__list-item">
-        <Link
-          to="organizations"
-          activeClass="active"
-          spy={true}
-          smooth={true}
+        <HashLink
+          smooth
+          to="/#organizations"
+          activeClassName="active"
           className="nav__list-link"
           onClick={closeMenuHandler}
         >
           Fundacja i organizacje
-        </Link>
+        </HashLink>
       </li>
       <li className="nav__list-item">
-        <Link
-          to="contact"
-          activeClass="active"
-          spy={true}
-          smooth={true}
+        <HashLink
+          smooth
+          to="/#contact"
+          activeClassName="active"
           className="nav__list-link"
           onClick={closeMenuHandler}
         >
           Kontakt
-        </Link>
+        </HashLink>
       </li>
     </ul>
   );
 };
 
 export default NavLinks;
+// import { Link } from "react-scroll";
+// import "./NavLinks.scss";
+
+// const NavLinks = ({ isMobile, closeMobileMenu }) => {
+//   const closeMenuHandler = () => {
+//     if (typeof closeMobileMenu === "function" && isMobile === true) {
+//       closeMobileMenu();
+//     }
+//   };
+//   return (
+//     <ul className="nav__list">
+//       <li className="nav__list-item">
+//         <Link
+//           to="start"
+//           activeClass="active"
+//           spy={true}
+//           smooth={true}
+//           className="nav__list-link"
+//           onClick={closeMenuHandler}
+//         >
+//           Start
+//         </Link>
+//       </li>
+//       <li className="nav__list-item">
+//         <Link
+//           to="steps"
+//           activeClass="active"
+//           spy={true}
+//           smooth={true}
+//           className="nav__list-link"
+//           onClick={closeMenuHandler}
+//         >
+//           O co chodzi?
+//         </Link>
+//       </li>
+//       <li className="nav__list-item">
+//         <Link
+//           to="about"
+//           activeClass="active"
+//           spy={true}
+//           smooth={true}
+//           className="nav__list-link"
+//           onClick={closeMenuHandler}
+//         >
+//           O nas
+//         </Link>
+//       </li>
+//       <li className="nav__list-item">
+//         <Link
+//           to="organizations"
+//           activeClass="active"
+//           spy={true}
+//           smooth={true}
+//           className="nav__list-link"
+//           onClick={closeMenuHandler}
+//         >
+//           Fundacja i organizacje
+//         </Link>
+//       </li>
+//       <li className="nav__list-item">
+//         <Link
+//           to="contact"
+//           activeClass="active"
+//           spy={true}
+//           smooth={true}
+//           className="nav__list-link"
+//           onClick={closeMenuHandler}
+//         >
+//           Kontakt
+//         </Link>
+//       </li>
+//     </ul>
+//   );
+// };
+
+// export default NavLinks;
