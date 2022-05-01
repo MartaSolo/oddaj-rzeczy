b4eb83110287fe902f9748cdeb7043ad02935a5e
MartaSolo <marciamarcia@o2.pl>
2022-05-01 16:30:50 +0200

    Added styling to About component

--- src/components/organisms/About/About.js
+++ src/components/organisms/About/About.js
@@ -1,9 +1,26 @@
+import BgImage from "../../atoms/BgImage";
 import "./About.scss";
 
 const About = () => {
   return (
     <section id="about" className="about">
-      <div>About component section</div>
+      <div className="about__content">
+        <div className="about__content-text">
+          <h2 className="about__content-title">O nas</h2>
+          <div className="about__text-decoration"></div>
+          <p className="about__content-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quidem
+            ab velit odio aliquid ullam facilis quasi consequuntur quae
+            exercitationem? Amet aperiam dolorem reprehenderit consequatur?
+          </p>
+        </div>
+      </div>
+      <BgImage
+        divClassName="about__content-image"
+        scr={require("../../../assets/people.jpg")}
+        alt="group of people"
+        imgClassName="about__image-img"
+      />
     </section>
   );
 };
