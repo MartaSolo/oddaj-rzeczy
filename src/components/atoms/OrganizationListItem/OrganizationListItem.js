71dfd9249d6d1dc51c0f8710cde7519a073c2e28
MartaSolo <marciamarcia@o2.pl>
2022-05-03 18:34:11 +0200

    adde dstyling to organizations list

--- src/components/atoms/OrganizationListItem/OrganizationListItem.js
+++ src/components/atoms/OrganizationListItem/OrganizationListItem.js
@@ -2,9 +2,22 @@ import "./OrganizationListItem.scss";
 
 const OrganizationListItem = ({ object }) => {
   return (
-    <div>
-      <div>List item</div>
-    </div>
+    <li className="organizations__listItem">
+      <h5 className="organizations__listItem-name">{object.name}</h5>
+
+      <div className="organizations__listItem-content">
+        <p className="organizations__listItem-description">
+          {object.description}
+        </p>
+        {object.collectedItems.map((item, index) => {
+          return (
+            <span key={index} className="organizations__listItem-item">
+              {item},{" "}
+            </span>
+          );
+        })}
+      </div>
+    </li>
   );
 };
 
--- src/data/organizationsData.js
+++ src/data/organizationsData.js
@@ -5,7 +5,7 @@ const organizations = [
       "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
     objects: [
       {
-        id: 1,
+        id: 11,
         name: 'Fundacja "Dbam o zdrowie"',
         description: "Pomoc osobom znajdującym się w trudnej sytuacji życiowej",
         collectedItems: [
@@ -17,19 +17,19 @@ const organizations = [
         ],
       },
       {
-        id: 2,
+        id: 22,
         name: 'Fundacja "Dla Dzieci"',
         description: "Pomoc dzieciom z ubogich rodzin",
         collectedItems: ["ubrania", "meble", "zabawki"],
       },
       {
-        id: 3,
+        id: 33,
         name: 'Fundacja "Bez domu"',
         description: "Pomoc dla osób nie podsiadających miejsca zamieszkania",
         collectedItems: ["ubrania", "meble", "jedzenie", "koce"],
       },
       {
-        id: 4,
+        id: 44,
         name: 'Fundacja "Ocalenie"',
         description:
           "Pomoc osobom znajdującym się w trudnej sytuacji życiowej, usługi socjalne, pomoc społeczna",
@@ -42,21 +42,21 @@ const organizations = [
         ],
       },
       {
-        id: 5,
+        id: 55,
         name: "Caritas Polska",
         description:
           "Pomoc dzieciom z ubogich rodzin, usługi socjalne, pomoc społeczna ",
         collectedItems: ["ubrania", "meble", "zabawki"],
       },
       {
-        id: 6,
+        id: 66,
         name: 'Fundacja "JiM"',
         description:
           " Usługi socjalne, pomoc społeczna, pomoc dla osób nie posiadających miejsca zamieszkania",
         collectedItems: ["ubrania", "meble", "jedzenie", "koce"],
       },
       {
-        id: 7,
+        id: 77,
         name: 'Fundacja "Dbam o zdrowie"',
         description: "Pomoc osobom znajdującym się w trudnej sytuacji życiowej",
         collectedItems: [
@@ -68,14 +68,14 @@ const organizations = [
         ],
       },
       {
-        id: 8,
+        id: 88,
         name: 'Fundacja "Przyszłość Dla Dzieci"',
         description:
           "Pomoc dzieciom z ubogich rodzin, usługi socjalne, pomoc społeczna",
         collectedItems: ["ubrania", "meble", "zabawki"],
       },
       {
-        id: 9,
+        id: 99,
         name: 'Fundacja "Beneficjum"',
         description:
           "Usługi socjalne, pomoc społeczna, pomoc dla osób nie posiadającyh miejsca zamieszkania",
@@ -90,7 +90,7 @@ const organizations = [
       "Pomagamy również wszelkim organizacjom pozarządowym i charytatywnym, które nie są Fundacjami. Są to nasi Partnerzy, który zrobią dobry pożytek z rzeczy, które do nich trafią.",
     objects: [
       {
-        id: 1,
+        id: 101,
         name: "Organizacja 1",
         description: "Pomoc osobom znajdującym się w trudnej sytuacji życiowej",
         collectedItems: [
@@ -102,19 +102,19 @@ const organizations = [
         ],
       },
       {
-        id: 2,
+        id: 102,
         name: "Organizacja 2",
         description: "Pomoc dzieciom z ubogich rodzin",
         collectedItems: ["ubrania", "meble", "zabawki"],
       },
       {
-        id: 3,
+        id: 103,
         name: "Organizacja 3",
         description: "Pomoc dla osób nie podsiadających miejsca zamieszkania",
         collectedItems: ["ubrania", "meble", "jedzenie", "koce"],
       },
       {
-        id: 4,
+        id: 104,
         name: "Organizacja 4",
         description:
           "Pomoc osobom znajdującym się w trudnej sytuacji życiowej, usługi socjalne, pomoc społeczna",
@@ -127,14 +127,14 @@ const organizations = [
         ],
       },
       {
-        id: 5,
+        id: 105,
         name: "Organizacja 5",
         description:
           "Pomoc dzieciom z ubogich rodzin, usługi socjalne, pomoc społeczna ",
         collectedItems: ["ubrania", "meble", "zabawki"],
       },
       {
-        id: 6,
+        id: 106,
         name: "Organizacja 6",
         description:
           " Usługi socjalne, pomoc społeczna, pomoc dla osób nie posiadających miejsca zamieszkania",
@@ -149,7 +149,7 @@ const organizations = [
       "Wspieramy lokalne zbiórki organizowane przez indywidualne osoby, którym zależy na dobru społeczności, w której żyją. Sam też możesz zorganizować taką zbiórkę i pomóc tym, którzy są najbliżej.",
     objects: [
       {
-        id: 1,
+        id: 201,
         name: "Zbiórka lokalna 1",
         description: "Pomoc osobom znajdującym się w trudnej sytuacji życiowej",
         collectedItems: [
@@ -161,13 +161,13 @@ const organizations = [
         ],
       },
       {
-        id: 2,
+        id: 202,
         name: "Zbiórka lokalna 2",
         description: "Pomoc dzieciom z ubogich rodzin",
         collectedItems: ["ubrania", "meble", "zabawki"],
       },
       {
-        id: 3,
+        id: 203,
         name: "Zbiórka lokalna 3",
         description: "Pomoc dla osób nie podsiadających miejsca zamieszkania",
         collectedItems: ["ubrania", "meble", "jedzenie", "koce"],
