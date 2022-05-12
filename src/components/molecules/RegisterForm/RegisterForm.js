e0c8171e060e937bb28303d5ab1c5f673b9bbc88
MartaSolo <marciamarcia@o2.pl>
2022-05-12 15:07:07 +0200

    Changed validation fo RegisterForm and styling

--- src/components/molecules/RegisterForm/RegisterForm.js
+++ src/components/molecules/RegisterForm/RegisterForm.js
@@ -9,65 +9,95 @@ const RegisterForm = () => {
     password: "",
     password2: "",
   });
-  //   console.log("registerForm", registerForm);
+  console.log("registerForm", registerForm);
   const [registerFormError, setRegisterFormError] = useState({
     email: "",
     password: "",
     password2: "",
   });
-  //   console.log("registerFormError", registerFormError);
+  console.log("registerFormError", registerFormError);
+
+  const [registerFormTouched, setRegisterFormTouched] = useState({
+    email: false,
+    password: false,
+    password2: false,
+  });
+  console.log("registerFormTouched", registerFormTouched);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setRegisterForm((prev) => {
-      return {
-        ...prev,
-        [name]: value,
-      };
-    });
+    setRegisterForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    if (e.target.value !== "") {
+      setRegisterFormError((prev) => ({ ...prev, [e.target.name]: "" }));
+    }
   };
 
-  const registerFormValidation = () => {
-    let isValid = true;
+  const handleBlur = (e) => {
+    switch (e.target.name) {
+      case "email":
+        setRegisterFormTouched((prev) => ({ ...prev, email: true }));
+        if (
+          registerForm.email.length === 0 ||
+          !regexEmail.test(registerForm.email)
+        ) {
+          setRegisterFormError((prev) => ({
+            ...prev,
+            email: "Podany email jest nieprawidłowy!",
+          }));
+        }
+        break;
+      case "password":
+        setRegisterFormTouched((prev) => ({ ...prev, password: true }));
+        if (registerForm.password.length < 6) {
+          setRegisterFormError((prev) => ({
+            ...prev,
+            password: "Podane hasło jest za krótkie!",
+          }));
+        }
+        break;
+      case "password2":
+        setRegisterFormTouched((prev) => ({ ...prev, password2: true }));
+        if (
+          registerForm.password2.length === 0 ||
+          registerForm.password2 !== registerForm.password
+        ) {
+          setRegisterFormError((prev) => ({
+            ...prev,
+            password2: "Podane hasła są rózne!",
+          }));
+        }
+        break;
+      default:
+        console.log("input not included in the fucntion");
+    }
+  };
 
+  const isRegisterFormValid = () => {
     if (
-      registerForm.email.length === 0 ||
-      !regexEmail.test(registerForm.email)
+      !registerFormError.email &&
+      !registerFormError.password &&
+      !registerFormError.password2 &&
+      registerFormTouched.email === true &&
+      registerFormTouched.password === true &&
+      registerFormTouched.password2 === true &&
+      registerForm.email &&
+      registerForm.password &&
+      registerForm.password2
     ) {
-      setRegisterFormError((prev) => {
-        return { ...prev, email: "Podany email jest nieprawidłowy!" };
-      });
-      isValid = false;
+      return true;
+    } else {
+      return false;
     }
-    if (registerForm.password.length < 6) {
-      setRegisterFormError((prev) => {
-        return {
-          ...prev,
-          password: "Podane hasło jest za krótkie!",
-        };
-      });
-      isValid = false;
-    }
-    if (registerForm.password2 !== registerForm.password) {
-      setRegisterFormError((prev) => {
-        return {
-          ...prev,
-          password2: "Podane hasła są rózne!",
-        };
-      });
-      isValid = false;
-    }
-    return isValid;
   };
+  console.log("isRegisterFormValid", isRegisterFormValid());
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const isValid = registerFormValidation();
-    if (isValid) {
-      // send data to API
-      setRegisterForm({ email: "", password: "", password2: "" });
-      setRegisterFormError({ email: "", password: "", password2: "" });
+    if (!isRegisterFormValid()) {
+      return;
     }
+    // send data to API
+    setRegisterForm({ email: "", password: "", password2: "" });
+    setRegisterFormTouched({ email: false, password: false, password2: false });
   };
 
   return (
@@ -89,6 +119,7 @@ const RegisterForm = () => {
             autoComplete="username"
             value={registerForm.email}
             onChange={handleChange}
+            onBlur={handleBlur}
           ></input>
           {registerFormError.email && (
             <span className="register__error-email">
@@ -112,6 +143,7 @@ const RegisterForm = () => {
             autoComplete="current-password"
             value={registerForm.password}
             onChange={handleChange}
+            onBlur={handleBlur}
           ></input>
           {registerFormError.password && (
             <span className="register__error-password">
@@ -135,6 +167,7 @@ const RegisterForm = () => {
             autoComplete="current-password"
             value={registerForm.password2}
             onChange={handleChange}
+            onBlur={handleBlur}
           ></input>
           {registerFormError.password2 && (
             <span className="register__error-password2">
@@ -149,7 +182,11 @@ const RegisterForm = () => {
           className="register__button-login"
           text="Zaloguj się"
         />
-        <button className="register__button-register" type="submit">
+        <button
+          className="register__button-register"
+          type="submit"
+          disabled={!isRegisterFormValid()}
+        >
           Załóż konto
         </button>
       </div>
