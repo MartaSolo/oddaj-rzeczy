437926b84d6134a38757d643eb6592ba795ad090
MartaSolo <marciamarcia@o2.pl>
2022-05-19 15:45:36 +0200

    edited Organizations component, where instead of takin data from organizationsData.js now they are taken from firestore

--- src/components/organisms/Organizations/Organizations.js
+++ src/components/organisms/Organizations/Organizations.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import getOrganizations from "../../../data/getOrganizations";
 import Container from "../../atoms/Container";
 import Decoration from "../../atoms/Decoration";
 import OrganizationTypeButtons from "../../molecules/OrganizationTypeButtons";
 import OrganizationsList from "../../molecules/OrganizationsList";
+import { db } from "../../../firebase/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 import "./Organizations.scss";
 
 const Organizations = () => {
@@ -11,8 +12,24 @@ const Organizations = () => {
   const [organizationsData, setOrganizationsData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
 
+  const organizationsColRef = collection(db, "organizations");
+
   useEffect(() => {
-    setOrganizationsData(getOrganizations(organizationType));
+    const getAllOrganizations = async () => {
+      try {
+        const data = await getDocs(organizationsColRef);
+        const allOrganizations = data.docs.map((doc) => ({ ...doc.data() }));
+        const getOragnization = (type) => {
+          return allOrganizations.filter(
+            (organization) => organization.type === type
+          );
+        };
+        setOrganizationsData(getOragnization(organizationType));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllOrganizations();
   }, [organizationType]);
 
   return (
@@ -22,8 +39,8 @@ const Organizations = () => {
         <Decoration className="organizations__content-decoration" />
 
         <OrganizationTypeButtons
-          organizationType={organizationType}
           setCurrentPage={setCurrentPage}
+          organizationType={organizationType}
           setOrganizationType={setOrganizationType}
         />
 
@@ -49,3 +66,58 @@ const Organizations = () => {
 };
 
 export default Organizations;
+
+// -----------------------------------------------------
+// opcja jak daen pobierane są z "./data"
+
+// import { useState, useEffect } from "react";
+// import getOrganizations from "../../../data/getOrganizations";
+// import Container from "../../atoms/Container";
+// import Decoration from "../../atoms/Decoration";
+// import OrganizationTypeButtons from "../../molecules/OrganizationTypeButtons";
+// import OrganizationsList from "../../molecules/OrganizationsList";
+// import "./Organizations.scss";
+
+// const Organizations = () => {
+//   const [organizationType, setOrganizationType] = useState("fundation");
+//   const [organizationsData, setOrganizationsData] = useState([]);
+//   const [currentPage, setCurrentPage] = useState(1);
+
+//   useEffect(() => {
+//     setOrganizationsData(getOrganizations(organizationType));
+//   }, [organizationType]);
+
+//   return (
+//     <section id="organizations" className="organizations">
+//       <Container>
+//         <h2 className="organizations__content-title">Komu pomagamy?</h2>
+//         <Decoration className="organizations__content-decoration" />
+
+//         <OrganizationTypeButtons
+//           organizationType={organizationType}
+//           setCurrentPage={setCurrentPage}
+//           setOrganizationType={setOrganizationType}
+//         />
+
+//         {organizationsData.map((organization) => {
+//           return (
+//             <p
+//               className="organizations__content-description"
+//               key={organization.type}
+//             >
+//               {organization.description}
+//             </p>
+//           );
+//         })}
+
+//         <OrganizationsList
+//           organizationsData={organizationsData}
+//           currentPage={currentPage}
+//           setCurrentPage={setCurrentPage}
+//         />
+//       </Container>
+//     </section>
+//   );
+// };
+
+// export default Organizations;
